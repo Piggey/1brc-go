1340b259928dedd061248e054f8e46155292f1e1
Piggey <pepcio3320@gmail.com>
2024-03-24 14:39:56 +0100

    how fast can we go

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"os"
 	"runtime"
-	"strings"
-	"sync"
 )
 
 const (
@@ -31,52 +29,5 @@ func main() {
 	}
 	defer f.Close()
 
-	dataCh := readerThread(f, cpuCores)
-	resultCh := make(chan map[string]stationData, cpuCores)
-
-	var wg sync.WaitGroup
-	for i := 0; i < cpuCores; i++ {
-		wg.Add(1)
-		go func() {
-			workerThread(dataCh, resultCh, &wg)
-		}()
-	}
-	wg.Wait()
-	close(resultCh)
-
-	// collect and reduce data
-	resultMap := initResultMap(minimumTemperature, maximumTemperature)
-
-	for result := range resultCh {
-		for stationName, data := range result {
-			currentData := resultMap[stationName]
-			resultMap[stationName] = stationData{
-				min: min(data.min, currentData.min),
-				max: max(data.max, currentData.max),
-				sum: data.sum + currentData.sum,
-				cnt: data.cnt + currentData.cnt,
-			}
-		}
-	}
-
-	// print result
-	output := generateOutput(resultMap)
-	fmt.Println(output)
-}
-
-func generateOutput(resultMap map[string]stationData) string {
-	output := "{"
-
-	for _, stationName := range stationsSorted {
-		data := resultMap[stationName]
-		minim := float64(data.min) / 10
-		maxim := float64(data.max) / 10
-		mean := float64(data.sum) / 10 / float64(data.cnt)
-
-		output += fmt.Sprintf("%s=%.1f/%.1f/%.1f, ", stationName, minim, mean, maxim)
-	}
-
-	output, _ = strings.CutSuffix(output, ", ")
-	output += "}"
-	return output
+	readerThread(f, cpuCores)
 }
--- readerThread.go
+++ readerThread.go
@@ -9,36 +9,23 @@ const (
 	chunksize = 4 * 1024 * 1024
 )
 
-func readerThread(f *os.File, cores int) <-chan []byte {
-	ch := make(chan []byte, cores)
-
-	go func() {
-		defer close(ch)
-
-		var err error
-		var read, lastNewlineIdx, offset int
-		buf := make([]byte, chunksize)
-		for {
-			read, err = f.Read(buf[offset:])
-			if read == 0 && err == io.EOF {
-				break
-			}
-
-			datalen := read + offset
-			lastNewlineIdx = lastIndexByte(buf, '\n', datalen)
-
-			// references bad!!
-			bufCopy := make([]byte, len(buf))
-			copy(bufCopy, buf)
-			ch <- bufCopy[:lastNewlineIdx+1]
-
-			// move bytes that werent sent to front
-			offset = datalen - lastNewlineIdx - 1
-			copy(buf[:offset], buf[lastNewlineIdx+1:datalen])
+func readerThread(f *os.File, cores int) {
+	var err error
+	var read, lastNewlineIdx, offset int
+	buf := make([]byte, chunksize)
+	for {
+		read, err = f.Read(buf[offset:])
+		if read == 0 && err == io.EOF {
+			break
 		}
-	}()
 
-	return ch
+		datalen := read + offset
+		lastNewlineIdx = lastIndexByte(buf, '\n', datalen)
+
+		// move bytes that werent sent to front
+		offset = datalen - lastNewlineIdx - 1
+		copy(buf[:offset], buf[lastNewlineIdx+1:datalen])
+	}
 }
 
 func lastIndexByte(buf []byte, c byte, start int) int {
